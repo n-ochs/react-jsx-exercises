906e56ff409ec928d5a003b87af1912eb0d35aa8
Nicholas Ochs <nick.ochs@yahoo.com>
2021-01-11 18:53:46 -0500

    list-group complete

--- list-group/app.js
+++ list-group/app.js
@@ -1,9 +1,22 @@
 let ListGroupItem = () => {
-    return <span>Make this component render like a Bootstrap list group item: <a href="https://getbootstrap.com/docs/4.1/components/list-group/" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
+    return <div>
+        <ul class="list-group">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+    </div>
 }
 
 let ListGroup = () => {
-    return <span>Make this Component render a handful of ListGroupItems</span>
+    return <div>
+        <ListGroupItem />
+        <ListGroupItem />
+        <ListGroupItem />
+        <ListGroupItem />
+    </div>
 }
 
 
