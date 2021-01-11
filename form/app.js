4fa0650880a06b9e19bbf37db5f890d9d546fb4b
Nicholas Ochs <nick.ochs@yahoo.com>
2021-01-11 18:50:56 -0500

    form complete

--- form/app.js
+++ form/app.js
@@ -1,17 +1,38 @@
 let TextInput = () => {
-    return <span>Make this component render a styled text input tag</span>
+    return <div>
+        <form>
+            <label for="fname">Input</label>
+            <input type="text" id="fname" name="fname"></input><br></br>
+        </form>
+    </div>
 }
 
 let YesNoRadio = () => {
-    return <span>Make this component render two styled radio button side by side labelled "yes" and "no"</span>
+    // return <span>Make this component render two styled radio button side by side labelled "yes" and "no"</span>
+    return <div>
+        <input type="radio" id="yes" name="option" value="yes"></input>
+        <label for="yes">Yes</label>
+        <input type="radio" id="no" name="option" value="no"></input>
+        <label for="no">No</label>
+    </div>
 }
 
 let SubmitButton = () => {
-    return <span>Make this component render a styled button of type "submit"</span>
+    return <div>
+        <button type="submit">Submit</button>
+    </div>
 }
 
 let Form = () => {
-    return <span>Make this Component render a form tag with 3 text inputs, two yes/no radio buttons, and a submit button </span>
+    // return <span>Make this Component render a form tag with 3 text inputs, two yes/no radio buttons, and a submit button </span>
+    return <div>
+        <TextInput />
+        <TextInput />
+        <TextInput />
+        <YesNoRadio />
+        <YesNoRadio />
+        <SubmitButton />
+    </div>
 }
 
 
